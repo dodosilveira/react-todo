889d7a22d55bf37930411871db48745e3ca393be
dodosilveira <douglas.silveira@processmind.com.br>
2022-05-31 23:40:19 -0300

    make it better

---
+++ src/Routes.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Switch, Redirect } from 'react-router-dom';
+
+import { RouteWithLayout } from './components';
+import { Main as MainLayout, Minimal as MinimalLayout } from './layouts';
+
+import {
+  Dashboard as DashboardView,
+  UserList as UserListView,
+  SignIn as SignInView,
+  NotFound as NotFoundView
+} from './views';
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Redirect
+        exact
+        from="/"
+        to="/dashboard"
+      />
+      <RouteWithLayout
+        component={DashboardView}
+        exact
+        layout={MainLayout}
+        path="/dashboard"
+      />
+      <RouteWithLayout
+        component={UserListView}
+        exact
+        layout={MainLayout}
+        path="/tarefas"
+      />
+      <RouteWithLayout
+        component={SignInView}
+        exact
+        layout={MinimalLayout}
+        path="/login"
+      />
+      <RouteWithLayout
+        component={NotFoundView}
+        exact
+        layout={MinimalLayout}
+        path="/not-found"
+      />
+      <Redirect to="/not-found" />
+    </Switch>
+  );
+};
+
+export default Routes;
